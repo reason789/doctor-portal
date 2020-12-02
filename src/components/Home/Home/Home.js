ae95c09d3fe24be162ff0f251c6c18cb88fd75ac
shopnil2 <rahadul134@gmail.com>
2020-12-02 06:54:49 +0600

    done frontend

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Blogs from '../Blogs/Blogs';
+import Contact from '../Contact/Contact';
+import Doctor from '../Doctor/Doctor';
+import Doctors from '../Doctors/Doctors';
+import FeaturedService from '../FeaturedService/FeaturedService';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
+import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Services></Services>
+            <FeaturedService></FeaturedService>
+            <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Blogs></Blogs>
+            <Doctors></Doctors>
+            <Contact></Contact>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Home/Services/Services.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Services.css';
+import fluoride from '../../../images/Flourida.png';
+import cavity from '../../../images/Cavity.png';
+import whitening from '../../../images/whitening.png';
+import ServiceDetail from '../ServiceDetail/ServiceDetail';
+
+const serviceData = [
+    {
+        title: 'Fluorida Treatment',
+        img: fluoride
+    },
+    {
+        title: 'Cavity Filling',
+        img: cavity
+    },
+    {
+        title: 'Teeth Whitening',
+        img: whitening
+    },
+]
+
+const Services = () => {
+    return (
+        <section className="services-container mt-5">
+            <div className="text-center">
+                <h5 style={{ color: '#1CC7C1' }}>OUR SERVICES</h5>
+                <h2>Services We Provide</h2>
+            </div>
+            <div className="d-flex justify-content-center">
+                <div className="row w-75 mt-5 pt-5">
+                    {
+                        serviceData.map(service => <ServiceDetail service={service}></ServiceDetail>)
+                    }
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Services;  
